79f75fa85c536cda8f84d95a818e1b6319523d7b
pipisebastian <yrt7998@pusan.ac.kr>
2023-06-30 00:51:24 +0900

    design : 테이블 80, 50% 이상 폰트 컬러 추가
    #15

--- src/components/TrashCanTable.tsx
+++ src/components/TrashCanTable.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+import { Text } from "react-native";
 import { DataTable } from "react-native-paper";
+
+import { theme } from "../core/theme";
 
 const optionsPerPage = [2, 3, 4];
 
@@ -13,9 +16,9 @@ const TrashCanTable = () => {
   }, [itemsPerPage]);
 
   return (
-    <DataTable style={{ marginTop: 60 }}>
+    <DataTable style={{ marginTop: 0 }}>
       <DataTable.Header>
-        <DataTable.Title>.</DataTable.Title>
+        <DataTable.Title> </DataTable.Title>
         <DataTable.Title>일반</DataTable.Title>
         <DataTable.Title>캔/병</DataTable.Title>
         <DataTable.Title>플라스틱</DataTable.Title>
@@ -24,24 +27,32 @@ const TrashCanTable = () => {
 
       <DataTable.Row>
         <DataTable.Cell>1층</DataTable.Cell>
-        <DataTable.Cell>100</DataTable.Cell>
+        <DataTable.Cell>
+          <Text style={{ color: theme.colors.error }}>100</Text>
+        </DataTable.Cell>
         <DataTable.Cell>20</DataTable.Cell>
-        <DataTable.Cell>90</DataTable.Cell>
-        <DataTable.Cell>95</DataTable.Cell>
+        <DataTable.Cell>
+          <Text style={{ color: theme.colors.error }}>90</Text>
+        </DataTable.Cell>
+        <DataTable.Cell>10</DataTable.Cell>
       </DataTable.Row>
 
       <DataTable.Row>
         <DataTable.Cell>2층</DataTable.Cell>
-        <DataTable.Cell>50</DataTable.Cell>
-        <DataTable.Cell>60</DataTable.Cell>
+        <DataTable.Cell>40</DataTable.Cell>
+        <DataTable.Cell>
+          <Text style={{ color: theme.colors.warning }}>60</Text>
+        </DataTable.Cell>
         <DataTable.Cell>25</DataTable.Cell>
         <DataTable.Cell>0</DataTable.Cell>
       </DataTable.Row>
 
       <DataTable.Row>
         <DataTable.Cell>3층</DataTable.Cell>
-        <DataTable.Cell>50</DataTable.Cell>
-        <DataTable.Cell>60</DataTable.Cell>
+        <DataTable.Cell>
+          <Text style={{ color: theme.colors.warning }}>60</Text>
+        </DataTable.Cell>
+        <DataTable.Cell>30</DataTable.Cell>
         <DataTable.Cell>25</DataTable.Cell>
         <DataTable.Cell>0</DataTable.Cell>
       </DataTable.Row>
@@ -49,7 +60,7 @@ const TrashCanTable = () => {
       <DataTable.Row>
         <DataTable.Cell>4층</DataTable.Cell>
         <DataTable.Cell>50</DataTable.Cell>
-        <DataTable.Cell>60</DataTable.Cell>
+        <DataTable.Cell>10</DataTable.Cell>
         <DataTable.Cell>25</DataTable.Cell>
         <DataTable.Cell>0</DataTable.Cell>
       </DataTable.Row>
