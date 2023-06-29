e1b9f613a996795c9a234bc22bba0b814c6854cb
pipisebastian <yrt7998@pusan.ac.kr>
2023-06-30 00:50:00 +0900

    design : 알람 UI 구성
    #15

---
+++ src/screens/Notification.tsx
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import Background from "../components/Background";
+import Button from "../components/Button";
+import { View, Text, Image } from "react-native";
+import { Switch } from "react-native-paper";
+
+import Icon from "react-native-vector-icons/Ionicons";
+
+import { theme } from "../core/theme";
+
+const Notification = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(true);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignSelf: "center",
+        alignItems: "center",
+        width: "100%",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          height: 60,
+          alignItems: "center",
+          backgroundColor: "#fff",
+          padding: 15,
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>앱 푸시</Text>
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      </View>
+      {/*  */}
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          padding: 14,
+          backgroundColor: "#f1f5f9",
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: "#fff",
+            padding: 10,
+            alignItems: "center",
+            flexDirection: "row",
+            borderRadius: 10,
+            marginBottom: 8,
+          }}
+        >
+          <Icon
+            size={25}
+            style={{ paddingRight: 8 }}
+            name="ios-information-circle-sharp"
+            color={theme.colors.error}
+          />
+          <View
+            style={{
+              flex: 1,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 1,
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "600" }}>
+                201 제도관
+              </Text>
+              <Text style={{ fontSize: 9 }}> 10분 전</Text>
+            </View>
+            <Text style={{ fontSize: 12 }}>일반 쓰레기가 가득 찼습니다!</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: "#fff",
+            padding: 10,
+            alignItems: "center",
+            flexDirection: "row",
+            borderRadius: 10,
+            marginBottom: 8,
+          }}
+        >
+          <Icon
+            size={25}
+            style={{ paddingRight: 8 }}
+            name="ios-warning"
+            color={theme.colors.warning}
+          />
+          <View
+            style={{
+              flex: 1,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 1,
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "600" }}>
+                201 제도관
+              </Text>
+              <Text style={{ fontSize: 9 }}> 2시간 전</Text>
+            </View>
+            <Text style={{ fontSize: 12 }}>
+              일반 쓰레기가 50% 이상 찼습니다!
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: "#fff",
+            padding: 10,
+            alignItems: "center",
+            flexDirection: "row",
+            borderRadius: 10,
+            marginBottom: 8,
+          }}
+        >
+          <Icon
+            size={25}
+            style={{ paddingRight: 8 }}
+            name="ios-warning"
+            color={theme.colors.warning}
+          />
+          <View
+            style={{
+              flex: 1,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 1,
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "600" }}>
+                303 건설관
+              </Text>
+              <Text style={{ fontSize: 9 }}> 3시간 전</Text>
+            </View>
+            <Text style={{ fontSize: 12 }}>
+              일반 쓰레기가 50% 이상 찼습니다!
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Notification;
