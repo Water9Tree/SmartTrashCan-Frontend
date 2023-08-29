a408a776b3488d2e891a3517cc0cfc643abbbc39
pipisebastian <yrt7998@pusan.ac.kr>
2023-08-29 21:55:15 +0900

    feat : 사용안하는 자세히보기 모달창 삭제
    #17

--- src/screens/Swipe.tsx
+++ src/screens/Swipe.tsx
@@ -1,11 +1,8 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { View, Text, ScrollView } from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
-import TrashCanTable from "../components/TrashCanTable";
-import Button from "../components/Button";
 
-import BuildingDetailModal from "../components/BuildingDetailModal";
 import { theme } from "../core/theme";
 
 interface SwipeProps {
@@ -17,11 +14,6 @@ const Swipe = ({ selectedBuilding }: SwipeProps) => {
   //여기 코드내에서, id -> 건물 모든 쓰레기통 정보 불러올것임
   const [temp, setTemp] = React.useState("");
 
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const handleDeleteBtnlick = () => {
-    setShowDeleteModal(true);
-  };
-
   React.useEffect(() => {
     //ㅎㅎ이건 걍 귀찮아서 박았음
     if (selectedBuilding === 201) setTemp("제도관");
@@ -30,12 +22,6 @@ const Swipe = ({ selectedBuilding }: SwipeProps) => {
     if (selectedBuilding === 506) setTemp("인문대");
     if (selectedBuilding === 708) setTemp("경암체육관");
   }, [selectedBuilding]);
-
-  const modalizeRef = useRef<any>(null);
-
-  const onOpen = () => {
-    modalizeRef.current?.open();
-  };
 
   // 80%이상 alert창 ui(빨간색)
   const render80Message = (text: string) => (
@@ -124,20 +110,6 @@ const Swipe = ({ selectedBuilding }: SwipeProps) => {
             >
               {selectedBuilding} {temp}
             </Text>
-            <Button
-              style={{
-                right: 4,
-                top: 10,
-                position: "absolute",
-              }}
-              onPress={handleDeleteBtnlick}
-            >
-              자세히
-            </Button>
-            <BuildingDetailModal
-              visible={showDeleteModal}
-              setVisible={setShowDeleteModal}
-            />
           </View>
           <View style={{ padding: 15 }}>
             <ScrollView>
